d351febf4b535cde4ae6ed8ade05c79ff20ca3c5
hareesh3jampala <hareesh3jampala@gmail.com>
2025-09-23 00:03:31 +0530

    some message

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# everthing in shell is considerd as string
 NUMBER1=100
 NUMBER2=200
 NAME=DevOps
@@ -7,3 +8,7 @@ NAME=DevOps
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
+
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All Leaders: ${LEADERS}[@]"
