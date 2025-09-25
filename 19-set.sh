ded2044067054358012a26caa9ea00550993c367
hareesh3jampala <hareesh3jampala@gmail.com>
2025-09-26 00:21:55 +0530

    some message

--- 19-set.sh
+++ 19-set.sh
@@ -6,7 +6,7 @@ set -e
 #     echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
 # }
 
-#trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello.."
 echo "Before error.."
